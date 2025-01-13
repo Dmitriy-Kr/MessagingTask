0206419c34429c97e35ec7d821de94f59579039e
Dmitriy Kraskovskyy <kraskovskyydmitriy@gmail.com>
2025-01-13 18:37:31 +0200

    Added Error handling for messages

--- workload/src/main/java/org/gym/workload/config/JmsConfig.java
+++ workload/src/main/java/org/gym/workload/config/JmsConfig.java
@@ -1,8 +1,14 @@
 package org.gym.workload.config;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
@@ -14,8 +20,19 @@ import java.util.Map;
 @EnableJms
 public class JmsConfig {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(JmsConfig.class);
+
+    @Value("${spring.activemq.broker-url}")
+    private String brokerUrl;
+
+    @Value("${spring.activemq.username}")
+    private String user;
+
+    @Value("${spring.activemq.password}")
+    private String password;
+
     @Bean
-    public MessageConverter jacksonJmsMessageConverter(){
+    public MessageConverter jacksonJmsMessageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
         converter.setTypeIdPropertyName("_type");
@@ -23,5 +40,28 @@ public class JmsConfig {
         typeIdMap.put("WorkloadMessage", org.gym.workload.message.WorkloadMessage.class);
         converter.setTypeIdMappings(typeIdMap);
         return converter;
+    }
+
+    @Bean
+    public CachingConnectionFactory connectionFactory() {
+
+        CachingConnectionFactory factory = new CachingConnectionFactory(
+                new ActiveMQConnectionFactory(user, password, brokerUrl)
+        );
+        factory.setClientId("Workload");
+        factory.setSessionCacheSize(100);
+        return factory;
+    }
+
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory());
+        factory.setMessageConverter(jacksonJmsMessageConverter());
+        factory.setErrorHandler(t -> {
+            LOGGER.info("Handling error in listening for messages, error: {}", t.getMessage());
+        });
+        return factory;
     }
 }
